e20c16e6fe47f62f4de2b282aed3b9ab11352d11
Alfredo <mate.alfredo98@gmail.com>
2026-05-31 11:36:31 +0200

    ref: small refactor

--- core/dynamic_programming/leet_322/src/main/java/Solution.java
+++ core/dynamic_programming/leet_322/src/main/java/Solution.java
@@ -4,19 +4,19 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
 
         int[] coinCombinations = new int[amount + 1];
-        Arrays.fill(coinCombinations, Integer.MAX_VALUE);
+        Arrays.fill(coinCombinations, amount + 1);
 
         coinCombinations[0] = 0;
         for (int i = 1; i < coinCombinations.length; i++) {
             for (int coin : coins) {
 
-                if (i-coin >= 0 && coinCombinations[i-coin] != Integer.MAX_VALUE) {
+                if (i-coin >= 0) {
                     coinCombinations[i] = Math.min(coinCombinations[i], coinCombinations[i-coin] + 1);
                 }
 
             }
         }
 
-        return coinCombinations[amount] == Integer.MAX_VALUE ? -1 : coinCombinations[amount];
+        return coinCombinations[amount] == amount + 1 ? -1 : coinCombinations[amount];
     }
 }
