c8392d6017efe3b740cf4cdb110fc58c7fc8a5da
Alfredo <mate.alfredo98@gmail.com>
2026-06-09 23:26:35 +0200

    feat: dfs + backtracking solution

---
+++ core/backtracking/leet_784/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String STRING = "a1b2";
+        Solution solution = new Solution();
+        System.out.println(solution.letterCasePermutation(STRING));
+
+    }
+}
